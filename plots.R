2cbbe2cec96eeb200942599a6b081f9c08e0943c
Brian Mason <brian.dale.mason@gmail.com>
2016-04-04 21:38:24 -0700

    Add bare bones plots
    For total running costs and cost per household.

---
+++ plots.R
@@ -0,0 +1,19 @@
+source('main.R')
+
+draw_plot <- function(data, title, x, y, filename) {
+  png(filename=filename)
+  plot(data, main=title, xlab=x, ylab=y, type='b')
+  dev.off()
+}
+
+draw_plot(total_running_cost,
+     title='Total Running Costs',
+     x='Empties (Households)',
+     y='Total running cost (RWF)',
+     filename='total_running_cost.png')
+
+draw_plot(cost_per_household,
+     title='Cost Per Household',
+     x='Empties (Households)',
+     y='Cost per household (RWF)',
+     filename='cost_per_household.png')
